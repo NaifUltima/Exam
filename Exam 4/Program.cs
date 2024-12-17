e352bcf5393fd137e6c937a6543e433c8f28e22d
Naif Alhusainan <nwef999@outlook.com>
2024-12-17 18:29:17 +0300

    q4

---
+++ Exam 4/Program.cs
@@ -0,0 +1,14 @@
+﻿public class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Write 5 numbers to get the average");
+        int[] Numbers = [5];
+        for(int i= 0; i < 5; i++ )
+        {
+        int.Parse(Console.ReadLine());
+        Console.WriteLine(Numbers[i]);
+        }
+        
+    }
+}
