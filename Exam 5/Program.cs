759696bc0d7212a064fec824c9087f1259eff938
Naif Alhusainan <nwef999@outlook.com>
2024-12-17 18:29:30 +0300

    q5

---
+++ Exam 5/Program.cs
@@ -0,0 +1,30 @@
+﻿public class Program
+{
+    static void Main()
+    {
+        string UserInput;
+        Console.WriteLine("What is youre grade?");
+        UserInput = Console.ReadLine();
+        
+        if(UserInput == "A" ) 
+        {
+            Console.WriteLine("Excellent");
+        }
+        else if(UserInput == "B")
+        {
+            Console.WriteLine("Very good");
+        }
+        else if(UserInput == "C")
+        {
+            Console.WriteLine("Good");
+        }
+        else if(UserInput == "D")
+        {
+            Console.WriteLine("Needs Improvment");
+        }
+        else if(UserInput == "F")
+        {
+            Console.WriteLine("Fail");
+        }
+    }
+}
