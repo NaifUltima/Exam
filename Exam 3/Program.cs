c3e81f7100a5a81c70d8afa8b25bc0e100b15f2f
Naif Alhusainan <nwef999@outlook.com>
2024-12-17 18:29:02 +0300

    q3

---
+++ Exam 3/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Program
+{
+    static void Main()
+    {
+        int UserInput1 = int.Parse(Console.ReadLine());
+        int UserInput2 = int.Parse(Console.ReadLine());
+
+        if( UserInput1> UserInput2)
+        {
+            Console.WriteLine($"{UserInput1} is larger");
+        }
+        else if (UserInput1 < UserInput2)
+        {
+            Console.WriteLine($"{UserInput2} is larger");
+        }
+
+    }
+}
